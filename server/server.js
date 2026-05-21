1284578d6cff2764bffd4b7b841e70bb07a56bb9
Edward <edwardwinata@gmail.com>
2026-05-21 22:36:37 +0700

    create server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const taskRoutes = require("./routes/tasks");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", taskRoutes);
+
+app.listen(5000, () => {
+  console.log(
+    "Server running on port 5000."
+  );
+});
