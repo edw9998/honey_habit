ced8d239949a7c3efb885124f27fb87d9c02d95e
Edward <edwardwinata@gmail.com>
2026-05-22 15:01:39 +0700

    connected habits table routers to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const taskRoutes = require("./routes/tasks");
+const habitRoutes = require("./routes/habits");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
+app.use("/habits", habitRoutes);
 
 app.listen(5000, () => {
   console.log(
