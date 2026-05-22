ad651ccc03669c74ecf605d2569f927d1344a7a8
Edward <edwardwinata@gmail.com>
2026-05-22 15:21:32 +0700

    used left and right dashboard view model

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,22 @@
-import Navbar from "../components/Navbar";
 import MascotCard from "../components/MascotCard";
 import TaskList from "../components/TaskList";
 import HabitTracker from "../components/HabitTracker";
 
 export default function Dashboard() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-yellow-100 to-pink-100">
-      <Navbar />
 
-      <div className="p-8 grid lg:grid-cols-2 gap-8">
+  return (
+    <div className="p-8 bg-gradient-to-b from-yellow-100 to-pink-100 min-h-screen">
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        {/* LEFT SIDE */}
         <MascotCard />
-        <TaskList />
-        <HabitTracker />
+        {/* RIGHT SIDE */}
+        <div className="flex flex-col gap-6">
+          <TaskList />
+          <HabitTracker />
+        </div>
       </div>
+
     </div>
   );
-  
 }
